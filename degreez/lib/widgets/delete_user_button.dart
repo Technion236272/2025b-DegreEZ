e189b3b1d7f27628a6b5eb2893c28ce510704d62
MoamenKassem <moamenkassem@campus.technion.ac.il>
2025-06-10 01:11:39 +0300

    added Deleting user page

--- degreez/lib/widgets/delete_user_button.dart
+++ degreez/lib/widgets/delete_user_button.dart
@@ -1,4 +1,5 @@
 import 'package:degreez/color/color_palette.dart';
+import 'package:degreez/pages/deleting_account_page.dart';
 import 'package:degreez/providers/course_provider.dart';
 import 'package:degreez/providers/login_notifier.dart';
 import 'package:degreez/providers/student_provider.dart';
@@ -28,19 +29,76 @@ class _DeleteUserButtonState extends State<DeleteUserButton> {
                 borderRadius: BorderRadius.circular(8),
               ),
             ),
-            onPressed: () async {
-              final rootNavigator = Navigator.of(context, rootNavigator: true);
-
-              await courseProvider.deleteStudentAndCourses(studentProvider.student!.id);
-              await logInNotifier.deleteUser();
-
-              rootNavigator.pushNamedAndRemoveUntil('/', (route) => false);
-            },
+            onPressed: () {confirmationPopUp();},
             icon: Icon(Icons.delete_forever, color: Colors.white),
             label: Text('Delete User Permanently'),
           ),
         );
       },
     );
+  }
+
+  confirmationPopUp(){
+    return showDialog(
+    context: context,
+    barrierDismissible: false, // Prevent dismissing by tapping outside
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text(
+          'Are you sure?',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: AppColorsDarkMode.secondaryColor,
+          ),
+        ),
+        content: const SingleChildScrollView(
+          child: Text(
+            'This action cannot be undone.\n\n'
+            'If you proceed, your account will be permanently deleted along with all associated data.',
+            style: TextStyle(
+              fontSize: 14,
+              color: AppColorsDarkMode.secondaryColor,
+            ),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(); // Cancel and close the dialog
+            },
+            child: const Text(
+              'Cancel',
+              style: TextStyle(
+                color: AppColorsDarkMode.secondaryColorDim,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              Navigator.of(context).push(MaterialPageRoute(
+      fullscreenDialog: true,
+      builder: (_) => DeletingAccountPage(),
+    ));
+              final rootNavigator = Navigator.of(context, rootNavigator: true);
+
+              await context.read<CourseProvider>().deleteStudentAndCourses(context.read<StudentProvider>().student!.id);
+
+              await context.read<LogInNotifier>().deleteUser();
+
+              rootNavigator.pushNamedAndRemoveUntil('/', (route) => false);
+            },
+            child: const Text(
+              'Delete',
+              style: TextStyle(
+                color: AppColorsDarkMode.secondaryColor,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ],
+      );
+    },
+  );
   }
 }
